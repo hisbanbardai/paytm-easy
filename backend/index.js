1aafba71c93c1007f85e18baa360048d35686186
Hisban Shiraz <hisbanbardai@gmail.com>
2024-11-20 07:55:46 -0700

    Code to create a server and listen to requests

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,9 @@
 const express = require("express");
+require("dotenv").config();
+require("cors");
 
+const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
