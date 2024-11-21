6c7fbf8a6e3738e2246ddaa6455a0ac5c562ea18
Hisban Shiraz <hisbanbardai@gmail.com>
2024-11-21 10:28:21 -0700

    Add express.json middleware

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.json());
+
 app.use("/api/v1", rootRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
