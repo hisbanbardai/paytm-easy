87d61192cf19c42ba32ccde356e8a8442c606307
Hisban Shiraz <hisbanbardai@gmail.com>
2024-11-21 10:09:57 -0700

    Add a new user router

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 require("cors");
-const mainRouter = require("./routes/index");
+const rootRouter = require("./routes/index");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1", mainRouter);
+app.use("/api/v1", rootRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
