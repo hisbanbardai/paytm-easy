33c1ab48cbb693e7dfe376a7e091a8c4dbac587b
Hisban Shiraz <hisbanbardai@gmail.com>
2024-11-21 10:04:57 -0700

    Import the created router object and route all requests from /api/v1 to it

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 require("cors");
+const mainRouter = require("./routes/index");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1", mainRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
