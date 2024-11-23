fe3a6188c316704239af3f3e9bdccbe2f4c45ba9
Hisban Shiraz <hisbanbardai@gmail.com>
2024-11-23 16:51:50 -0700

    Add password column to users table

--- backend/db/migrations/20241119171249_create_users_table.sql
+++ backend/db/migrations/20241119171249_create_users_table.sql
@@ -7,6 +7,7 @@ CREATE TABLE
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
     created_at TIMESTAMP
     WITH
       TIME ZONE DEFAULT CURRENT_TIMESTAMP
