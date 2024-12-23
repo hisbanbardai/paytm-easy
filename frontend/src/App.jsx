4976123eec0b8fdd145327a3a516ff29bb4710f8
Hisban Shiraz <hisbanbardai@gmail.com>
2024-12-23 13:33:55 -0700

    Create components for pages in the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/Signin";
+import SendMoney from "./pages/SendMoney";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/SendMoney.jsx
@@ -0,0 +1,3 @@
+export default function SendMoney() {
+  return <p>Send Money</p>;
+}
