be4d10f0dbb527e9be696c3fbb0a620e0643b9f3
Hisban Shiraz <hisbanbardai@gmail.com>
2024-12-25 14:55:31 -0700

    Add basic html structure of the dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,26 @@
 export default function Dashboard() {
-  return <p className="bg-slate-400">Dashboard page</p>;
+  return (
+    <div>
+      <header>
+        <h1>PayTM App</h1>
+        <p>Hello U</p>
+      </header>
+      <main>
+        <section>
+          <h2>Your balance Rs 10,000</h2>
+        </section>
+        <section>
+          <h2>Users</h2>
+          <input type="text" placeholder="Search users..." />
+          <div>
+            <div>
+              <span>H</span>
+              <p>Hisban Shiraz</p>
+              <button>Send Money</button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
