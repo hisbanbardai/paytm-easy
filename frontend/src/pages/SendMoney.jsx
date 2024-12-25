8260f0083c0fbd019266aba1a09367492fe82f88
Hisban Shiraz <hisbanbardai@gmail.com>
2024-12-25 15:38:20 -0700

    Add basic html structure of the send money page

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -1,3 +1,19 @@
+import Avatar from "../components/Avatar";
+import Heading from "../components/Heading";
+
 export default function SendMoney() {
-  return <p>Send Money</p>;
+  return (
+    <main>
+      <section>
+        <Heading label={"Send Money"} />
+        <div>
+          <Avatar value={"A"} />
+          <p>Friend's name</p>
+          <p>Amount in Rs.</p>
+          <input type="text" placeholder="Enter amount" />
+          <button>Initiate Transfer</button>
+        </div>
+      </section>
+    </main>
+  );
 }
