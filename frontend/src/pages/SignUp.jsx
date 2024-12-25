93e21667090af70aad4bec35a9853cdddf2a46f4
Hisban Shiraz <hisbanbardai@gmail.com>
2024-12-25 11:57:38 -0700

    Add basic html structure for sign up page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,3 +1,23 @@
 export default function SignUp() {
-  return <p>Sign up page</p>;
+  return (
+    <div>
+      <div>
+        <h2>Sign up</h2>
+        <p>Enter your information to create an account</p>
+
+        <label htmlFor="">First Name</label>
+        <input type="text" placeholder="John" />
+        <label htmlFor="">Last Name</label>
+        <input type="text" placeholder="Doe" />
+        <label htmlFor="">Email</label>
+        <input type="text" placeholder="test@gmail.com" />
+        <label htmlFor="">Password</label>
+        <input type="text" placeholder="123456" />
+
+        <button>Sign up</button>
+
+        <p>Already have an account? Sign in</p>
+      </div>
+    </div>
+  );
 }
