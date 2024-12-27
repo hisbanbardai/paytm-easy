4284ed091eb400cad31d42ec485d1d39bad4d305
Hisban Shiraz <hisbanbardai@gmail.com>
2024-12-27 07:53:23 -0700

    Add a route to validate the token

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
 const userRouter = require("./user");
 const accountRouter = require("./account");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.use("/user", userRouter);
 router.use("/account", accountRouter);
+
+router.get("/auth/validate", authMiddleware, (req, res) => {
+  if (req.userId) {
+    return res.status(200).json({ message: "Token is valid" });
+  }
+});
 
 module.exports = router;
