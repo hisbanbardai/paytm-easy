30ccfb00be7be4c2e1dcceb648d7383e379f1630
Hisban Shiraz <hisbanbardai@gmail.com>
2024-12-27 10:58:49 -0700

    Fetch balance of the user from the backend and display it on the dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
 import Balance from "../components/Balance";
 import TopBar from "../components/TopBar";
 import Users from "../components/Users";
+import axios from "axios";
 
 export default function Dashboard() {
+  const [balance, setBalance] = useState(null);
+
+  useEffect(() => {
+    async function getBalance() {
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/account/balance",
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+
+      setBalance(response.data.balance);
+    }
+
+    getBalance();
+  }, []);
+
   return (
     <div className="flex flex-col gap-10">
       <TopBar />
       <main className="px-9 flex flex-col gap-6">
-        <Balance value={"10,000"} />
+        <Balance value={balance} />
         <Users />
       </main>
     </div>
